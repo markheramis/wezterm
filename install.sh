74300b296a1fefd9ce9e2c90071f9799cae2e48b
Mark Heramis <chumheramis@gmail.com>
2025-09-11 04:42:35 +0800

    fix string
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ if [ -d "$HOME/.config/wezterm" ]; then
   mv "$HOME/.config/wezterm" "$HOME/.config/wezterm.bak"
 fi
 
-ln -s "$(pwd)/config $HOME/.config/wezterm
+ln -s $(pwd)/config $HOME/.config/wezterm
