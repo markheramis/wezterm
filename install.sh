b89d80ecf94b83a3ba6b17bd3537e969b771f583
Mark Heramis <chumheramis@gmail.com>
2025-09-11 04:41:04 +0800

    restructure config and update installer
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-# Move .config/wezterm to .config/wezterm.bak if it exists
 if [ -d "$HOME/.config/wezterm" ]; then
   mv "$HOME/.config/wezterm" "$HOME/.config/wezterm.bak"
 fi
-# Link wezterm to .config/wezterm
-ln -s "$(pwd)/wezterm" "$HOME/.config/wezterm"
+
+ln -s "$(pwd)/config $HOME/.config/wezterm
